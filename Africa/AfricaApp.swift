8f183cf72a63fddf8e4cc1a92286b6eba1d368d3
Grishechko, Dmitriy <ext_dgrishechko@rencredit.ru>
2023-05-03 19:58:44 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by anduser on 03.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
